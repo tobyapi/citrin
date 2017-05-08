2e5485372200303272f97db30b845499dbb65d2e
TobiasGSmollett <landofscala@gmail.com>
2017-05-08 15:59:49 +0900

    Add directory traverser

---
+++ src/citrin/core/traverser.cr
@@ -0,0 +1,23 @@
+module Citrin::Core
+  class Traverser
+    def all_file_path(root_path)
+
+      result = Array(String).new
+      queue = Array(String).new
+      queue << root_path
+
+      until (current = queue.first).nil?
+        queue.shift
+
+        if File.file? current
+          result << current
+        else
+          Dir.foreach(current) do |entry|
+            queue << current + '/' + entry
+          end
+        end
+      end
+      result
+    end
+  end
+end
