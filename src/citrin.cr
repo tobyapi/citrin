01de6c5140cbc58413d9972abe5c552b2f198fce
TobiasGSmollett <landofscala@gmail.com>
2017-05-10 11:35:21 +0900

    Add getting clone_url from request

--- src/citrin.cr
+++ src/citrin.cr
@@ -1,3 +1,17 @@
+require "json"
 require "kemal"
+require "./citrin/utils/*"
+
+include Citrin::Utils
+
+get "/" do
+  "Hello World!"
+end
+
+post "/push" do |env|
+  clone_url = env.params.json["repository"].as(Hash(String, JSON::Type)).fetch("clone_url").as(String)
+  Git.clone clone_url
+  {result: "success"}.to_json
+end
 
 Kemal.run
